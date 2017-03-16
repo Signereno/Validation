93de2ce4017aa70c7de11840cadb7f75486e263a
vPolland <vegard.polland@unipluss.no>
2017-03-16 09:12:06 +0100

    Added validation sample client

---
+++ ValidationExampleClient/ValidationExampleClient/Program.cs
@@ -0,0 +1,77 @@
+﻿//######################################################################//
+//            This example uses the following nuget packages:           //
+//                                                                      //
+//            - IdentiSign.Validation.Client                            //
+//            - IdentiSign.Validation.Models                            //
+//                                                                      //              
+//######################################################################//
+
+
+using System;
+using System.IO;
+using IdentiSign.Validation.Client;
+using IdentiSign.Validation.Client.ValidationClient.NO;
+using IdentiSign.Validation.Models.NO.BankID;
+
+namespace ValidationExampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If you have an account insert your credentials here, you can do up to 10 requests per day without an account
+            Guid? accountId = null;
+            string oauthClientId = null;
+            string oauthSecret = null;
+
+
+            //#################################################################################################//
+            //                                    SDO (Norwegian bankid)                                       //      
+            //                                ---------------------------------                                //          
+            //#################################################################################################//
+            
+
+
+            //Create new client for norwegain bank id
+            INoBankidValidationClient noClient = new NoBankidValidationClient(accountId, oauthClientId, oauthSecret);
+
+            var fileData = File.ReadAllBytes("App_Data/signedtxt.sdo");
+            var originalText = File.ReadAllBytes("App_Data/originaltxt.txt"); // The original of the signed text document (You can include this in the request (dataToValidate) 
+                                                                              // to check that the original text matches the signed text)
+
+            //  Validate SDO                                   
+            //--------------------                                       
+
+            Console.WriteLine("Validating SDO document, please wait...");
+            var request = new ValidateSDORequest()
+            {
+                sdoData = Convert.ToBase64String(fileData),
+                dataToValidate = Convert.ToBase64String(originalText) //Optional
+            };
+            
+            var result = noClient.ValidateSdo(request, isProd: false);
+
+            Console.WriteLine(Extensions.SerializeAndFormat(result));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            // Validate and Parse SDO                                 
+            //---------------------------------  
+            Console.WriteLine("Parsing SDO document, please wait...");
+
+            var parseRequest = new ParseSDORequest()
+            {
+                sdo = Convert.ToBase64String(fileData)
+            };
+
+            var parseResult = noClient.ValidateAndParseSdo(parseRequest, isProd: false);
+
+            Console.WriteLine(Extensions.SerializeAndFormat(parseResult));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
